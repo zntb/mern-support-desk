ec2ca8e128c9752c8c0cb40669f0dc7dcfb705b3
zntb <zntbdev@proton.me>
2024-02-25 11:54:54 +0200

    Basic Express Server Setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Serves listen on PORT ${PORT}`);
+});
