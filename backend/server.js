c07ca9fbdb21e909e40081e6935b239bf2ea7acb
zntb <zntbdev@proton.me>
2024-02-25 13:33:24 +0200

    Add Error Exception Handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const errorHandler = require('./middleware/errorMiddleware.js');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
@@ -10,6 +14,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes.js'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Serves listen on PORT ${PORT}`);
