70d205b664abcd20c401361ad6d0a542a0d8aa7a
zntb <zntbdev@proton.me>
2024-02-25 17:32:17 +0200

    Add Protect Routes for  Authentication

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel.js');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user from token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      // Check if a user was found
+      if (!req.user) {
+        res.status(401);
+        throw new Error('Not authorized');
+      }
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not authorized');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+});
+
+module.exports = { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/userController.js');
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController.js');
+const { protect } = require('../middleware/authMiddleware.js');
 
 router.post('/', registerUser);
-
 router.post('/login', loginUser);
+router.get('/me', protect, getMe);
 
 module.exports = router;
