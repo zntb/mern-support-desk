9524bb0c058b3d7f1df0b72cc0a9d60b31a6f46c
zntb <zenetibi@yahoo.com>
2024-02-25 13:49:09 +0200

    Connect To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db.js');
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
