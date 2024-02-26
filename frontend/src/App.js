7b4b1a3f74732fde40d716628f79ab387e55127b
zntb <zntbdev@proton.me>
2024-02-26 12:43:27 +0200

    Add Header Initial Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
