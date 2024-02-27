a466ca1498e3a16115cbc40b64419f5580d14537
zntb <zenetibi@yahoo.com>
2024-02-27 13:18:10 +0200

    Add Route Guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Header from './components/Header.jsx';
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
+import NewTicket from './pages/NewTicket.jsx';
 
 function App() {
   return (
@@ -17,6 +19,14 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
