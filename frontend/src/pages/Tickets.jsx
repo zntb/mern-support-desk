3f514ad9b8ffdd9c0b91e98a70680af2da0e5fb4
zntb <zntbdev@proton.me>
2024-02-27 16:11:44 +0200

    Listing Tickets In UI

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getTickets } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
 import BackButton from '../components/BackButton';
+import TicketItem from '../components/TicketItem.jsx';
 
 function Tickets() {
   const { tickets } = useSelector((state) => state.tickets);
@@ -21,6 +22,17 @@ function Tickets() {
     <>
       <BackButton />
       <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
     </>
   );
 }
