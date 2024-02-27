a47f61f7b8ab6c77642ab5bf24b5face024fa504
zntb <zenetibi@yahoo.com>
2024-02-27 12:47:44 +0200

    Add Single Ticket Update Delete Functionality

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const {
   getTickets,
   createTicket,
+  getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../controllers/ticketController.js');
 
 const { protect } = require('../middleware/authMiddleware.js');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
