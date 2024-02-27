d8712a6b5b7f0d9648682eedcd34b5e788b0628c
zntb <zenetibi@yahoo.com>
2024-02-27 15:34:24 +0200

    Create Ticket Functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -1,16 +1,45 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import ticketService from './ticketService';
+import { extractErrorMessage } from '../../utils';
 
 const initialState = {
   tickets: null,
   ticket: null,
 };
 
+// Create new ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(extractErrorMessage(error));
+    }
+  }
+);
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
   extraReducers: (builder) => {
-    //
+    // builder
+    //   .addCase(getTickets.pending, (state) => {
+    //     state.ticket = null;
+    //   })
+    //   .addCase(getTickets.fulfilled, (state, action) => {
+    //     state.tickets = action.payload;
+    //   })
+    //   .addCase(getTicket.fulfilled, (state, action) => {
+    //     state.ticket = action.payload;
+    //   })
+    //   .addCase(closeTicket.fulfilled, (state, action) => {
+    //     state.ticket = action.payload;
+    //     state.tickets = state.tickets.map((ticket) =>
+    //       ticket._id === action.payload._id ? action.payload : ticket
+    //     );
+    //   });
   },
 });
 
