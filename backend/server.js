4b0c4a09e5fddf017431b5e8c6bc6667ed091cc2
zntb <zenetibi@yahoo.com>
2024-02-28 17:51:52 +0200

    Preparation for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('colors');
 require('dotenv').config();
@@ -18,13 +19,24 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API' });
-});
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes.js'));
 app.use('/api/tickets', require('./routes/ticketRoutes.js'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 app.use(errorHandler);
 
